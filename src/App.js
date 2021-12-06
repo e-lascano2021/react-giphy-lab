cefaa14fb1a9cae80debcffedee9b0043444aa06
e.lascano <emilas5678@gmail.com>
2021-12-06 17:37:02 -0500

    render a gif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import DisplayGif from './Components/DisplayGif';
 
 function App() {
+  const [gifData, setGifData] = useState('')
+
+  useEffect(() => {
+    let gifUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=KKwPyLZHZ2E0TUwsFHaQOkPyb4goDiYG&limit=1&rating=g';
+
+    const makeApiCall = () => {
+      fetch(gifUrl)
+      .then(res => res.json())
+      .then(data => {
+        setGifData(data.data[0].images.original.url)
+        console.log("gifdata", gifData)
+      });
+    }
+    makeApiCall()
+  },[])
+
+
   return (
     <div className="App">
+      <DisplayGif gif={gifData}/>
     </div>
   );
 }
