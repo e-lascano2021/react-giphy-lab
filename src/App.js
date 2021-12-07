6428560b0fd47f721e6196185797e695d69696f7
e.lascano <emilas5678@gmail.com>
2021-12-07 17:41:14 -0500

    completed lab

--- src/App.js
+++ src/App.js
@@ -2,32 +2,54 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 import DisplayGif from './Components/DisplayGif';
 import RandomizeGif from './Components/RandomizeGif';
+import Form from './Components/Form'
 
 function App() {
   const [gifData, setGifData] = useState('')
+  const [apiUrl, setApiUrl] = useState("https://api.giphy.com/v1/gifs/random?api_key=KKwPyLZHZ2E0TUwsFHaQOkPyb4goDiYG&tag=&rating=g")
+  const [searchCount, setSearchCount] = useState(0)
 
-  let gifUrl = 'https://api.giphy.com/v1/gifs/random?api_key=KKwPyLZHZ2E0TUwsFHaQOkPyb4goDiYG&tag=&rating=g';
 
-  const makeApiCall = () => {
-    fetch(gifUrl)
-    .then(res => res.json())
-    .then(data => {
-      setGifData(data.data.images.original.url)
-      console.log("gifdata", gifData)
-    });
+
+
+  const handleSubmit = (searchTerm) => {
+    if (typeof searchTerm == 'string') {
+      setApiUrl(`https://api.giphy.com/v1/gifs/search?api_key=KKwPyLZHZ2E0TUwsFHaQOkPyb4goDiYG&q=${searchTerm}&limit=1&offset=0&rating=g&lang=en`)
+    } else {
+      setApiUrl(`https://api.giphy.com/v1/gifs/random?api_key=KKwPyLZHZ2E0TUwsFHaQOkPyb4goDiYG&tag=&rating=g`)
+    }
+    setSearchCount(searchCount + 1)
   }
 
+
+
+
   useEffect(() => {
-    makeApiCall()
-  },[])
+    const makeApiCall = (url) => {
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log('Gif data:', data)
+        if (data.data.length) {
+          setGifData(data.data[0].images.original.url)
+        } else {
+          setGifData(data.data.images.original.url)
+        }
+      })
+    }
+    makeApiCall(apiUrl)
+  }, [searchCount])
+
 
 
   return (
     <div className="App">
+      <Form handleSubmit={handleSubmit} />
       <DisplayGif gif={gifData}/>
-      <RandomizeGif handleClick={makeApiCall}/>
+      <RandomizeGif handleClick={handleSubmit}/>
     </div>
-  );
+  )
 }
+
 
 export default App;
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,17 +1,36 @@
-import React from "react";
 
-// function Form() {
-//   return(
-//     <>
-//       <form onSubmit={handleSubmit}>
-//         <label htmlFor="movieTitle">Title:</label>
-//         <input
-//           id="movieTitle"
-//           type="text"
-//           onChange={handleChange}
-//         />
-//         <input type="submit" value="Find Movie Info" />
-//       </form>
-//     </>
-//   )
-// }
+import React, { useState } from 'react'
+
+function Form(props) {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = e => {
+    console.log('handleSubmit clicked');
+    e.preventDefault();
+    props.handleSubmit(search)
+    setSearch('')
+  };
+
+  const handleChange = e => {
+    console.log('handleChange clicked');
+    const searchInput =  e.target.value
+    setSearch(searchInput)
+  };
+
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="searchInput">Search:</label>
+          <input
+            id="searchInput"
+            type="text"
+            value={search}
+            onChange={handleChange}
+          />
+          <input type="submit" value="Search for gif" />
+        </form>
+      </>
+    );
+}
+
+export default Form
